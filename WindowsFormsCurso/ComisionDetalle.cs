8d223297c9cffde42ae9aa8b4c4c4b68bf259a3d
Ramiro-Mc <204116304+Ramiro-Mc@users.noreply.github.com>
2025-09-22 23:58:07 -0300

    agrego Forms ComisionesLista y ComisionDetalle

---
+++ WindowsFormsCurso/ComisionDetalle.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DTOs;
+using WindowsForms;
+
+namespace WindowsFormsCurso
+{
+    public partial class ComisionDetalle : Form
+    {
+        private ComisionDTO comision;
+        private FormMode mode;
+
+        public ComisionDTO Comision
+        {
+            get { return comision; }
+            set
+            {
+                comision = value;
+                // this.SetCurso();
+            }
+        }
+
+        public FormMode Mode
+        {
+            get
+            {
+                return mode;
+            }
+            set
+            {
+                //SetFormMode(value);
+            }
+        }
+        public ComisionDetalle()
+        {
+            InitializeComponent();
+        }
+    }
+}
